ef00f604178b3d1c215859e8a00156f7d36b36a6
Marina Bulic <marinabulic1@gmail.com>
2017-10-30 19:27:55 +0100

    REST error handling implemented

---
+++ app/src/main/java/hr/foi/vodickulturnihdogadanja/utils/RESTError.java
@@ -0,0 +1,26 @@
+package hr.foi.vodickulturnihdogadanja.utils;
+
+/**
+ * Created by marbulic on 10/30/2017.
+ */
+
+public class RESTError {
+    private int errorNumber;
+    private String errorMessage;
+
+    public int getErrorNumber() {
+        return errorNumber;
+    }
+
+    public void setErrorNumber(int errorNumber) {
+        this.errorNumber = errorNumber;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
--- app/src/main/java/hr/foi/vodickulturnihdogadanja/utils/RESTErrorDecoderUtils.java
+++ app/src/main/java/hr/foi/vodickulturnihdogadanja/utils/RESTErrorDecoderUtils.java
@@ -1,5 +1,8 @@
 package hr.foi.vodickulturnihdogadanja.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 
 import okhttp3.ResponseBody;
@@ -9,12 +12,33 @@ import okhttp3.ResponseBody;
  */
 
 public class RESTErrorDecoderUtils {
-    public static final int SERVER_INTERNAL_EXCEPTION_ERRR  = 950;
 
-    public static int decodeError(ResponseBody body){
-        //decode
-        //switch return
 
-        return SERVER_INTERNAL_EXCEPTION_ERRR;
+    public static final int POST_JSON_DATA_FORMAT_ERROR=10001;
+    public static final int POST_MISSING_PARAMETERS=10002;
+    public static final int POST_INVALID_DATA_VALUES=10003;
+    public static final int INVALID_TOKEN=10004;
+    public static final int SERVER_INTERNAL_EXCEPTION_ERR  = 10005;
+    public static final int GET_MISSING_PARAMETERS=10006;
+    public static final int GET_INVALID_DATA_VALUES=10007;
+    public static final int GET_ID_NOT_FOUND=10008;
+    public static final int POST_VALIDATION_FAILED=10009;
+    public static final int REGISTRATION_EXISTING_USERNAME=10010;
+    public static final int REGISTRATION_EXISTING_EMAIL=10011;
+    public static final int LOGIN_WRONG_CREDENTIALS  = 10012;
+    public static final int GET_RESURCE_NOT_FOUND=10013;
+
+
+
+    public static RESTError decodeError(ResponseBody body){
+        Gson gson = new Gson();
+        String json = null;
+        try {
+            json = body.string();
+            return gson.fromJson(json, RESTError.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
