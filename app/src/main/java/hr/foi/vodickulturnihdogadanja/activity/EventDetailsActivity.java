49835e033e24df0e1a5d72545bcd5522b7271c6a
matsisl <matsisl@foi.hr>
2018-01-10 19:58:30 +0100

    Added new fragments "CommentFragment", "DetailsFragment" and "MapFragment". "EventDetailsActivity" changed for display tabs(fragments).

--- app/src/main/java/hr/foi/vodickulturnihdogadanja/activity/EventDetailsActivity.java
+++ app/src/main/java/hr/foi/vodickulturnihdogadanja/activity/EventDetailsActivity.java
@@ -2,7 +2,9 @@ package hr.foi.vodickulturnihdogadanja.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -31,6 +33,9 @@ import hr.foi.vodickulturnihdogadanja.SocialNetworkSharingContainer;
 import hr.foi.vodickulturnihdogadanja.SocialNetworkSharingManager;
 import hr.foi.vodickulturnihdogadanja.SocialNetworkSharingManagerListener;
 import hr.foi.vodickulturnihdogadanja.TwitterSharingManager;
+import hr.foi.vodickulturnihdogadanja.fragments.CommentsFragment;
+import hr.foi.vodickulturnihdogadanja.fragments.EventDetailsFragment;
+import hr.foi.vodickulturnihdogadanja.fragments.MapFragment;
 import hr.foi.vodickulturnihdogadanja.interactor.impl.EventDetailsInteractorImpl;
 import hr.foi.vodickulturnihdogadanja.interactor.impl.FavoriteInteractorImpl;
 import hr.foi.vodickulturnihdogadanja.model.CommentModel;
@@ -45,194 +50,74 @@ import hr.foi.vodickulturnihdogadanja.view.EventDetailsView;
 import hr.foi.voidckulturnihdogadanja.FacebookSharingManager;
 
 
-public class EventDetailsActivity extends AppCompatActivity implements EventDetailsView,SocialNetworkSharingManagerListener, SocialNetworkSharingContainer {
-    @BindView(R.id.event_details_name)
-    TextView txtEventName;
-    @BindView(R.id.event_details_description)
-    TextView txtEventDescription;
-    @BindView(R.id.event_details_begin)
-    TextView txtEventBegin;
-    @BindView(R.id.event_details_end)
-    TextView txtEventEnd;
-    @BindView(R.id.event_details_link)
-    TextView txtEventLink;
-    @BindView(R.id.event_details_location)
-    TextView txtEventLocation;
-    @BindView(R.id.event_details_price)
-    TextView txtEventPrice;
-    @BindView(R.id.event_details_img)
-    ImageView imgEvent;
-    @BindView(R.id.btn_share)
-    Button btnShare;
-    @BindView(R.id.favoriteCheckBox)
-    CheckBox favoriteCheckBox;
-    @BindView(R.id.btn_twitter)
-    Button btnTwitter;
-   @BindView(R.id.img_like)
-    ImageView imgLike;
-    @BindView(R.id.img_dislike)
-    ImageView imgDislike;
-
-
-    EventDetailsPresenter dp;
-    int eventId=-1;
-    SocialNetworkSharingManager shareManager;
-    EventModel event;
-    TwitterAuthClient client;
-    CommentPresenter cp;
-
+public class EventDetailsActivity extends AppCompatActivity {
+    public static EventDetailsActivity instance;
+    private CommentsFragment fragmentComments;
+    private EventDetailsFragment fragmentDetails;
+    private MapFragment fragmentMap;
+    private TabLayout allTabs;
+    public int eventId=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_event_details);
-        ButterKnife.bind(this);
-
-        EventDetailsPresenter edp = new EventDetailsPresenterImpl(new EventDetailsInteractorImpl(),new FavoriteInteractorImpl(), this);
-        this.dp=edp;
-
-
-        TokenModel token = LoggedUserData.getInstance().getTokenModel();
-
-        TwitterConfig config = new TwitterConfig.Builder(this)
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig("v0POVP7nGnIkhBZIs3YAjt8Lr", "AakBcoLnYc4WMzEFyYihwvRZwGynP2R5PP0diMw62L4yP0nEkf"))
-                .debug(true)
-                .build();
-        Twitter.initialize(config);
-
-        client = new TwitterAuthClient();
-        //make the call to login
-
-        shareManager = new FacebookSharingManager();
-        shareManager.setListener(this);
-
+        instance=this;
+        getAllWidgets();
+        bindWidgetsWithAnEvent();
+        setupTabLayout();
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Intent intent = getIntent();
-        eventId = intent.getIntExtra("id",-1);
-        if(eventId != -1)
+    private void getAllWidgets() {
+        allTabs = (TabLayout) findViewById(R.id.tabs);
+    }
+    private void setupTabLayout() {
+        Intent intent = this.getIntent();
+        eventId=intent.getIntExtra("id", -1);
+        Bundle bundle = new Bundle();
+        bundle.putInt("eventId", eventId);
+        fragmentDetails = new EventDetailsFragment();
+        fragmentComments = new CommentsFragment();
+        fragmentMap = new MapFragment();
+        fragmentComments.setArguments(bundle);
+        fragmentDetails.setArguments(bundle);
+        allTabs.addTab(allTabs.newTab().setText("DETALJI"),true);
+        allTabs.addTab(allTabs.newTab().setText("KOMENTARI"));
+        allTabs.addTab(allTabs.newTab().setText("MAPA"));
+    }
+    private void bindWidgetsWithAnEvent()
+    {
+        allTabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                setCurrentTabFragment(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+    private void setCurrentTabFragment(int tabPosition)
+    {
+        switch (tabPosition)
         {
-            dp.tryGetEventById(eventId);
+            case 0 :
+                replaceFragment(fragmentDetails);
+                break;
+            case 1 :
+                replaceFragment(fragmentComments);
+                break;
+            case 2:
+                replaceFragment(fragmentMap);
+                break;
         }
     }
-
-    @OnClick(R.id.btn_share)
-    public void click(){
-        shareManager = new FacebookSharingManager();
-        initSharing(shareManager);
-    }
-
-    private void initSharing(SocialNetworkSharingManager shareManager){
-        shareManager.setListener(this);
-        shareManager.setContainer(this);
-        shareManager.share(this, event.getEventId());
-    }
-
-
-    @OnClick(R.id.btn_twitter)
-    public void clickTwitter(){
-        shareManager = new TwitterSharingManager();
-        initSharing(shareManager);
-    }
-
-
-    @Override
-    public void ArrivedEvent(EventModel event) {
-        this.event=event;
-        txtEventName.setText(event.getName());
-        txtEventDescription.setText(event.getDescription());
-        txtEventBegin.setText(DateConverter(event.getBegin()));
-        if(event.getEnd()>0){
-            txtEventEnd.setText("-"+DateConverter(event.getEnd()));
-        }
-        else {
-            txtEventEnd.setText("");
-        }
-
-        txtEventLocation.setText(event.getLocation());
-        txtEventPrice.setText(String.valueOf(event.getPrice()));
-        txtEventLink.setText(event.getLink());
-        imgEvent.setImageBitmap(Base64Coding.decodeBase64(event.getPicture()));
-    }
-    private String DateConverter(Long date) {
-        if (date == 0) {
-            return "";
-        } else {
-            SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy. HH:mm");
-            Date d = new Date(date);
-            return df.format(d);
-        }
-    };
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //client.onActivityResult(requestCode, resultCode, data);
-        shareManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-
-    @OnClick(R.id.favoriteCheckBox)
-    public void favoriteCheckBoxClick(){
-        this.dp.tryAddFavorite(this.event.getEventId());
-    }
-
-    @Override
-    public void onSuccessAddedFavorite() {
-        Toast.makeText(this,"Dogadaj dodan u favorite!", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onSuccessAddedEvaluation() {
-        Toast.makeText(this,"Uspjesno ocijenjeno!", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onFailedAddedEvaluation() {
-        Toast.makeText(this,"Neuspjesno ocijenjeno!", Toast.LENGTH_LONG).show();
-    }
-
-    @OnClick(R.id.img_like)
-    public void clickLike(){
-        dp.tryAddEvaluation(1,LoggedUserData.getInstance().getTokenModel().getUserId(),event.getEventId());
-    }
-    @OnClick(R.id.img_dislike)
-    public void clickDislike(){
-        dp.tryAddEvaluation(0,LoggedUserData.getInstance().getTokenModel().getUserId(),event.getEventId());
-    }
-
-
-    @Override
-    public void shared() {
-        Toast.makeText(this,"Uspjesno podijeljeno", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void canceled() {
-        Toast.makeText(this,"Niste podijelili!", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void showFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.commit();
-        btnTwitter.setVisibility(View.GONE);
-        btnShare.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void hideFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.remove(fragment);
-        transaction.commit();
-        btnTwitter.setVisibility(View.VISIBLE);
-        btnShare.setVisibility(View.VISIBLE);
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_container, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
 }
---
+++ app/src/main/java/hr/foi/vodickulturnihdogadanja/fragments/CommentsFragment.java
@@ -0,0 +1,10 @@
+package hr.foi.vodickulturnihdogadanja.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by LEGION Y520 on 10.1.2018..
+ */
+
+public class CommentsFragment extends Fragment {
+}
