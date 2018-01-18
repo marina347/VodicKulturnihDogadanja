22d370586b9af47308aab9d445d48c269c3850c3
matsisl <matsisl@foi.hr>
2018-01-18 13:33:49 +0100

    Added Google map on MapFragment. S4F10

--- app/src/main/java/hr/foi/vodickulturnihdogadanja/fragments/MapFragment.java
+++ app/src/main/java/hr/foi/vodickulturnihdogadanja/fragments/MapFragment.java
@@ -1,10 +1,15 @@
 package hr.foi.vodickulturnihdogadanja.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import hr.foi.vodickulturnihdogadanja.R;
 
@@ -12,11 +17,23 @@ import hr.foi.vodickulturnihdogadanja.R;
  * Created by LEGION Y520 on 10.1.2018..
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private SupportMapFragment supportMapFragment;
+    GoogleMap map;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_map, null);
+        supportMapFragment = new SupportMapFragment();
+        getFragmentManager().beginTransaction().add(R.id.frame, supportMapFragment).commit();
         return rootView;
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        supportMapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 }
